2a4e1817728b6e651cba8ebb63853ed5bb419e39
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2023-12-10 11:28:05 +0600

    Add more fonts

--- lib/dto/font_family.dart
+++ lib/dto/font_family.dart
@@ -1,6 +1,9 @@
-enum FontFamilyOptions { alqalam, uthmanic }
+enum FontFamilyOptions { alqalam, uthmanic, alquranali, lateef, amiri }
 
 const Map<FontFamilyOptions, String> fontMap = {
   FontFamilyOptions.alqalam: "Al-Qalam",
   FontFamilyOptions.uthmanic: "Uthmanic",
+  FontFamilyOptions.alquranali: "AlQuranAli",
+  FontFamilyOptions.lateef: "Lateef",
+  FontFamilyOptions.amiri: "Amiri",
 };
