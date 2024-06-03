63ac4eab78db668fd806deca69a46d670bfbbdc6
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2024-06-03 23:20:46 +0800

    Only mark learned if last 2 attempt is correct

--- lib/repository/mcq_attempt_repo.dart
+++ lib/repository/mcq_attempt_repo.dart
@@ -5,13 +5,23 @@ import 'package:logging/logging.dart';
 const String tableQuizAttempt = 'mcq_attempts';
 const String columnWordId = 'wordId';
 const String columnIsCorrect = 'isCorrect';
+const String columnAttemptAt = 'attemptAt';
 
-class WordAttempt {
+class WordAttemptResult {
   final String wordId;
   int? totalCorrect;
   int? totalFailed;
 
-  WordAttempt({required this.wordId});
+  WordAttemptResult({required this.wordId});
+}
+
+class WordAttempt {
+  final String wordId;
+  bool isCorrect;
+  DateTime attemptAt;
+
+  WordAttempt(
+      {required this.wordId, required this.isCorrect, required this.attemptAt});
 }
 
 class MCQAttemptRepo extends RepoBase {
@@ -19,6 +29,7 @@ class MCQAttemptRepo extends RepoBase {
   final log = Logger('MCQAttemptRepo');
 
   recordAttempt(int wordId, isCorrect) async {
+    const int lastConsecutiveCorrect = 2;
     log.info('recordAttempt: $wordId $isCorrect');
 
     await insert(tableQuizAttempt, {
@@ -26,32 +37,60 @@ class MCQAttemptRepo extends RepoBase {
       'isCorrect': isCorrect ? 1 : 0,
     });
 
-    // Mark word as learned if corrected once
-    // Later need to check if the last 2 attempts were correct
     if (isCorrect) {
-      await WordRepo().markLearned(wordId);
+      int totaltLastCorrected = await getTotalLastConsecutiveCorrect(wordId);
+      if (totaltLastCorrected >= lastConsecutiveCorrect) {
+        await WordRepo().markLearned(wordId);
+      }
     }
+  }
+
+  Future<int> getTotalLastConsecutiveCorrect(int wordId) async {
+    List<WordAttempt> attempts = await getWordAttemptsForWord(wordId);
+    int totaltLastCorrected = 0;
+    for (var i = 0; i < attempts.length; i++) {
+      if (attempts[i].isCorrect) {
+        totaltLastCorrected++;
+      } else {
+        break;
+      }
+    }
+    return totaltLastCorrected;
   }
 
   getTotalWordIdsAttempted() async {
     return await query(tableQuizAttempt);
   }
 
+  Future<List<WordAttempt>> getWordAttemptsForWord(wordId) async {
+    var result = await query(tableQuizAttempt,
+        columns: [columnWordId, columnIsCorrect, columnAttemptAt],
+        where: '$columnWordId = ?',
+        whereArgs: [wordId],
+        orderBy: '$columnAttemptAt DESC');
+    return result
+        .map((row) => WordAttempt(
+            wordId: row[columnWordId].toString(),
+            isCorrect: row[columnIsCorrect] == 1,
+            attemptAt: DateTime.parse(row[columnAttemptAt].toString())))
+        .toList();
+  }
+
   // Not planning use this method as this would be very expensive
   @deprecated
-  Future<List<WordAttempt>> getWordAttemptsWithCount() async {
+  Future<List<WordAttemptResult>> getWordAttemptsWithCount() async {
     var result = await query(tableQuizAttempt,
         distinct: true,
         columns: [columnWordId, columnIsCorrect, 'count(*) as total'],
         groupBy: "$columnWordId, $columnIsCorrect");
 
-    Map<String, WordAttempt> wordsAttemptMap = {};
+    Map<String, WordAttemptResult> wordsAttemptMap = {};
     for (final row in result) {
       var wordId = row[columnWordId].toString();
-      wordsAttemptMap[wordId] ??= WordAttempt(wordId: wordId);
+      wordsAttemptMap[wordId] ??= WordAttemptResult(wordId: wordId);
 
-      WordAttempt attempt =
-          wordsAttemptMap[wordId] ?? WordAttempt(wordId: wordId);
+      WordAttemptResult attempt =
+          wordsAttemptMap[wordId] ?? WordAttemptResult(wordId: wordId);
       var total = int.parse(row['total'].toString());
 
       if (row[columnIsCorrect] != 0) {
@@ -62,7 +101,7 @@ class MCQAttemptRepo extends RepoBase {
       wordsAttemptMap[wordId] = attempt;
     }
 
-    var wordAttempts = List<WordAttempt>.of(wordsAttemptMap.values);
+    var wordAttempts = List<WordAttemptResult>.of(wordsAttemptMap.values);
     wordAttempts.sort((a, b) =>
         ((b.totalFailed ?? 0) - (b.totalCorrect ?? 0)) -
         ((a.totalFailed ?? 0) - (a.totalCorrect ?? 0)));
