d205210dbcd93dc1f7fcd5461777b72c8401c1eb
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2024-07-28 10:54:05 +0800

    Loaded examples, WIP

--- scripts/initialize-db.js
+++ scripts/initialize-db.js
@@ -10,10 +10,6 @@ class Stage extends Realm.Object {
   description;
   isComplete = false;
 
-  constructor(realm, description) {
-    super(realm, { description });
-  }
-
   static schema = {
     name: "Stage",
     primaryKey: "id",
@@ -29,10 +25,6 @@ class Lesson extends Realm.Object {
   id;
   stageId;
   name;
-
-  constructor(realm, description) {
-    super(realm, { description });
-  }
 
   static schema = {
     name: "Lesson",
@@ -54,10 +46,6 @@ class Word extends Realm.Object {
   arabic;
   meaning;
 
-  constructor(realm, description) {
-    super(realm, { description });
-  }
-
   static schema = {
     name: "Word",
     primaryKey: "id",
@@ -75,8 +63,28 @@ class Word extends Realm.Object {
   };
 }
 
+class Example extends Realm.Object {
+  ayahRef;
+  wordId;
+  arabic;
+  translation;
+  highlight;
+
+  static schema = {
+    name: "Example",
+    properties: {
+      ayahRef: "string",
+      wordId: "int",
+      arabic: "string",
+      translation: "string",
+      highlight: "string",
+    },
+  };
+}
+
+
 const localConfig = {
-  schema: [Stage, Lesson, Word],
+  schema: [Stage, Lesson, Word, Example],
   path: "assets/db.realm",
 };
 const realm = await Realm.open(localConfig);
@@ -101,15 +109,53 @@ async function getWords() {
   return await getData('Words');
 }
 
+async function getExamples() {
+  return await getData('Examples');
+}
+
+function getWordByArabic(id, arabic) {
+  if(id) {
+    return realm.objects('Word').filtered('id = $0', id)[0];
+  }
+  return realm.objects('Word').filtered('arabic = $0', arabic)[0];
+}
+
+function loadExamplesToDB(examples) {
+  let currentWordId;
+  examples.map(example => {
+    const isWordNewWord = example.ayahRef === '' && example.highlight === '';
+    if(isWordNewWord) {
+      const currentWord = getWordByArabic(example.wordId, example.word);
+      if (currentWord === undefined) {
+        console.log(`Word not found: ${example.word}`);
+        return;
+      }
+      currentWordId = currentWord.id;
+      return;
+    }
+
+    realm.create("Example", {
+      ayahRef: example.ayahRef,
+      wordId: currentWordId,
+      arabic: example.word,
+      translation: example.translation,
+      highlight: example.highlight,
+    }, "modified");
+  });
+}
+
+
 async function seed() {
   const stages = await getStages();
   const lessons = await getLessons();
   const words = await getWords();
+  const examples = await getExamples();
 
   realm.write(() => {
     stages.map(stage => upsertStage(stage));
     lessons.map(lesson => upsertLesson(lesson));
     words.map(word => upsertWord(word));
+    loadExamplesToDB(examples);
   });
 }
 
