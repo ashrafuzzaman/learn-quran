c8e6b610148922ab1e2327e7f5114661bcc516a9
AKM Ashrafuzzaman <ashrafuzzaman.g2@gmail.com>
2024-08-10 17:39:58 +0800

    Added schema for stage

--- app/models/Stage.ts
+++ app/models/Stage.ts
@@ -5,7 +5,18 @@ export class Stage extends Realm.Object {
   description!: string;
   isComplete = false;
 
-  static primaryKey = "id";
+  static primaryKey = 'id';
+  static schema = {
+    name: 'Stage',
+    primaryKey: 'id',
+    properties: {
+      id: 'int',
+      totalLessons: { type: 'int', default: 0 },
+      totalLessonsComplete: { type: 'int', default: 0 },
+      name: 'string',
+      description: 'string',
+    },
+  };
 
   constructor(realm: Realm, description: string) {
     super(realm, { description });
